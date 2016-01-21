b175983eb19a8c3e2cdfcd01c7033c2af8dd80b3
David <davidptracy@gmail.com>
2016-01-21 00:09:38 -0500

    Multiple Particles

---
+++ 03_02_MultipleParticles.py
@@ -0,0 +1,49 @@
+import rhinoscriptsyntax as rs
+import math
+import time
+import random
+
+class Particle:
+    def __init__(self, _startPoint ):
+        self.location = _startPoint
+        self.velocity = rs.VectorCreate([random.uniform(-1,1),random.uniform(-1,1),random.uniform(1,2)],[0,0,0])
+        self.acceleration = rs.VectorCreate([0,0,-0.005],[0,0,0])
+        self.lifeSpan = random.uniform(200,300)
+        
+    def run(self):
+        self.update()
+        self.display()
+        
+    def update(self):
+        self.velocity = rs.VectorAdd(self.velocity,self.acceleration)
+        self.location = rs.VectorAdd(self.location,self.velocity)
+        self.lifeSpan -= 2.0
+        
+    def display(self):
+        self.sphere = rs.AddSphere(self.location, 5.0)
+        
+    def isDead(self):
+        if (self.lifeSpan < 0.0):
+            return True
+        else:
+            return False
+
+
+particles = []
+
+#particle = Particle(rs.VectorCreate([20,20,20],[0,0,0]))
+
+for p in range(20):
+    p = Particle(rs.VectorCreate([20,20,20],[0,0,0])) 
+    particles.append(p)
+
+            
+for t in range(500):
+#   create a copy of the particles list first  
+#   So when you modify the original list,
+#   you do not modify the copy that you iterate over
+#   note: this is slow because it iterates over the entire list each time
+    for p in particles[:]:
+        p.run()
+        if p.isDead():
+            particles.remove(p)
